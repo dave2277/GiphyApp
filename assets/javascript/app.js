3a5485a6a72b38e667206ae1a29a4f2d40099b96
David Gray <davidgray@Davids-MacBook-Pro.local>
2017-06-14 11:37:15 -0600

    button builder functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,62 @@
 //Variables
 //============================================
-var authKey = ""//Giphy API Key goes here
+var authKey = "dc6zaTOxFJmzC"//Giphy API Key goes here
 
 var results = ""
 
-var queryURL = ""//Can we put in the entire query string or do we need to just create a base?
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=" + authKey//Can we put in the entire query string or do we need to just create a base?
+//Query parameters:
+//q:
+//limit:
+//rating: 
+
+var topics = ["camping", "hiking", "mountain biking", "snowboarding", "spelunking", "fly fishing", "sky diving", "rock climbing", "skiing", "snowshoeing", "jogging", "kayaking", "snorkeling", "hang gliding", " hot air ballooning"];
+
 
 //Functions
 //============================================
 
-function runQuery() {
+$( document ).ready(function() {
 
-	//AJAX Function
-	$.ajax({url: queryURL,
-		method: "GET"})
-	.done(function(response)){
-	console.log(response);
-	console.log(queryURL);
-	})
+function makeButtons() {
+
+	$("#buttons").empty();
+
+	for (i = 0; i < topics.length; i++) {
+
+		var activity = $("<button>");
+
+		activity.addClass = ("gifmaker");
+
+		activity.attr("data-name", topics[i]);
+
+		activity.text(topics[i]);
+
+		$("#buttons").append(activity);
+	}
+
 }
+makeButtons();
+
+$("#button").on("click", function() {
+
+event.preventDefault;
 
 
-
+});
 
 //Main Processes
 //============================================
+
+// function runQuery() {
+
+// 	//AJAX Function
+// 	$.ajax({url: queryURL,
+// 		method: "GET"})
+// 	.done(function(response)){
+// 	console.log(response);
+// 	console.log(queryURL);
+// 	})
+// }
+
+});
