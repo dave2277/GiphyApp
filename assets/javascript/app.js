d56ca35ad78278a7d007baa09b9e3664a30fb9fa
David Gray <davidgray@Davids-MacBook-Pro.local>
2017-06-17 07:44:14 -0600

    Functional except for behaviors when images are clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,19 @@
 //Variables
 //============================================
-var authKey = "dc6zaTOxFJmzC"//Giphy API Key goes here
-
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=" + authKey//Can we put in the entire query string or do we need to just create a base?
-//Query parameters:
-//q:
-//limit:
-//rating: 
-
+var rating;
 var topics = ["camping", "hiking", "mountain biking", "snowboarding", "spelunking", "fly fishing", "sky diving", "rock climbing", "skiing", "snowshoeing", "jogging", "kayaking", "snorkeling", "hang gliding", " hot air ballooning"];
-
 var activity;
-
 var buttonLabel;
+var stillGif;
+var animatedGif;
+var newGif;
 
 //Functions
 //============================================
 
 $( document ).ready(function() {
 
+//Loop through the topics array and make our buttons
 function makeButtons() {
 
 	$("#buttons").empty();
@@ -34,37 +29,68 @@ function makeButtons() {
 		activity.text(topics[i]);
 
 		$("#buttons").append(activity);
+		}
+
+	}
+	makeButtons();
+
+	//Display Gifs when clicking buttons
+	$(document).on("click", ".gifmaker", displayGif);
+
+		function displayGif() {
+
+			$("#gifs").empty();
+
+			buttonLabel = $(this).attr("data-name");
+
+			var authKey = "dc6zaTOxFJmzC"//Giphy API 
+			var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonLabel + "&limit=10" + "&api_key=" + authKey;
+
+			//API function
+			$.ajax({url: queryURL,
+			method: "GET"})
+			.done(function(response){
+
+			//Loop through results and get ratings and images
+			for (i = 0; i < response.data.length; i++) {
+
+			rating = response.data[i].rating;
+
+			stillGif = response.data[i].images.fixed_height_still.url;
+
+			animatedGif = response.data[i].images.fixed_height.url;
+
+			//Display images and ratings on the page
+			$("#gifs").append('<div class="stillPictures"> <span class="ratings">Rating: ' + rating  + '</span><br><img class="images" src=' + stillGif + '></div>');
+
+			$(".stillPictures").on("click", function() {
+
+			$(".images").attr('src', animatedGif);
+
+
+			});
+
+
+			}
+
+		})
 	}
 
-}
-makeButtons();
+	//Function for changing state of image on click
 
-$(document).on("click", ".gifmaker", function() {
+	//Add button from form input
+	$("#singlebutton").on("click", function(event) {
+		event.preventDefault();
 
-event.preventDefault;
+		newGif = $("#textinput").val().trim();
 
+		topics.push(newGif);
 
-buttonLabel = $(this).attr("data-name");
+		makeButtons();
 
-console.log(buttonLabel);
+	});
 
+	makeButtons();
 
-});
-
-
-
-//Main Processes
-//============================================
-
-// function runQuery() {
-
-// 	//AJAX Function
-// 	$.ajax({url: queryURL,
-// 		method: "GET"})
-// 	.done(function(response)){
-// 	console.log(response);
-// 	console.log(queryURL);
-// 	})
-// }
 
 });
